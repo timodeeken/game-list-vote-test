2c4ba4c3e41f13af5d8a38f69d2b9e9cedd225fc
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2023-02-03 14:29:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <div class="min-h-screen flex items-center">
+<body class="bg-gray-100">
+    <div class="min-h-screen flex items-center justify-center">
         <div class="bg-white p-6">
             Welcome on GameList Vote Script Test
-            
+
         </div>
     </div>
 </body>
