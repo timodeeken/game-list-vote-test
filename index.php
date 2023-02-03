e2e27a51aa2d32544755612c5917355cd1f976b7
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2023-02-03 14:48:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -34,29 +34,53 @@ if(!empty($_POST['post_value'])) {
 ?>
 <body class="bg-gray-100">
     <div class="min-h-screen flex flex-col items-center justify-center">
-        <div class="bg-white p-6 rounded w-1/2 shadow-lg mb-5">
+        <div class="bg-white p-6 rounded w-1/2 shadow-lg mb-5 text-center flex flex-col justify-center">
             Welcome on GameList Vote Script Test
 
             <div>
                 Click on the button to get a test vote link
             </div>
-            <?php if(empty($result)){ ?>
-            <form action="/" method="post">
-                <input type="hidden" name="post_value" value="<?php echo random_bytes(20) ?>">
-                <button type="submit" name="create_vote" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    Create Vote Link
-                </button>
-            </form>
-            <?php } ?>
-            <?php if (!empty($result)){ ?>
-                <a target="_blank" href="<?php echo $result->url ?>" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    Vote Now
-                </a>
-            <?php } ?>
+            <div class="w-32">
+                <?php if(empty($result)){ ?>
+                    <form action="/" method="post">
+                        <input type="hidden" name="post_value" value="<?php echo random_bytes(20) ?>">
+                        <button type="submit" name="create_vote" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                            Create Vote Link
+                        </button>
+                    </form>
+                <?php } ?>
+                <?php if (!empty($result)){ ?>
+                    <a target="_blank" href="<?php echo $result->url ?>" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        Vote Now
+                    </a>
+                <?php } ?>
+            </div>
         </div>
 
         <div class="bg-white p-6 rounded w-1/2 shadow-lg mb-5">
-            <code>sjkfdhlkgdls</code>
+            <h2>Installation Guid</h2>
+
+            <h4>Step 1</h4>
+            <p class="italic text-gray-400">Request Vote Link from our API</p>
+            <code>
+                $ch = curl_init();
+                $data = [
+                'token' => 'SERVER_TOKEN',
+                'username' => 'USERNAME',
+                'return_url' => 'RETURN_URL'
+                ];
+                curl_setopt($ch, CURLOPT_URL, 'https://game-list.eu/v1/vote');
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+                $headers = array();
+                $headers[] = 'Content-Type: application/x-www-form-urlencoded';
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+                $result = curl_exec($ch);
+                $result = json_decode($result);
+            </code>
         </div>
     </div>
 </body>
