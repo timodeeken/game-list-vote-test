74702cd1da89681077d5c5a1261254332ebfe9c3
Timo Deeken <53052110+timodeeken@users.noreply.github.com>
2023-02-03 14:43:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ if($_POST) {
             </form>
             <?php } ?>
             <?php if (!empty($result)){ ?>
-                <a href="<?php $result->url ?>" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                <a href="<?php echo $result->url ?>" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                     Vote Now
                 </a>
             <?php } ?>
